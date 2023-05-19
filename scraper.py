d0c971899a78bbd705c13f86f83a06752b8fe7dd
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2023-05-19 10:01:35 +0200

    improved scraper, added Atlas language, more words for other languages

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import requests
 from urllib.parse import unquote
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet
 import os
 
 
@@ -8,7 +8,8 @@ def __main__():
     URLS = [
         'https://nomanssky.fandom.com/wiki/Gek_(language)',
         'https://nomanssky.fandom.com/wiki/Korvax_(language)',
-        'https://nomanssky.fandom.com/wiki/Vy%27keen_(language)'
+        'https://nomanssky.fandom.com/wiki/Vy%27keen_(language)',
+        'https://nomanssky.fandom.com/wiki/Atlas_(language)'
     ]
 
     for url in URLS:
@@ -22,35 +23,65 @@ def __main__():
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        # Get the table from the HTML
-        table = soup.select('table.wikitable')[0]
-        table_body = table.find('tbody')
+        tables = soup.select('table.wikitable')
+        print(f'Tables found: {len(tables)}')
 
-        # Get the rows from the table
-        rows = table_body.find_all('tr')
+        data = None
 
-        # check if the directory exists
-        try:
-            os.mkdir('dict')
-        except FileExistsError:
-            pass
+        for table in tables:
+            table_rows = table.findAll('tr')[1:]
 
-        # Get the words from the rows
-        file = open(f'dict/{language}.txt', 'w')
+            # Get the rows from the table
+            if data:
+                data.extend(table_rows)
+            else:
+                data = table_rows
 
-        file.write(str(len(rows) - 1) + '\n')
+        # Store the rows in a file
+        storeRows(data if data else [], language)
 
-        for row in rows[1:]:
-            columns = row.find_all('td')
 
-            english = columns[0].text
-            foreign = columns[1].text
-            foreign_caps = columns[2].text
-            foreign_all_caps = columns[3].text
+def storeRows(rows, filename):
+    # check if the directory exists
+    try:
+        os.mkdir('dict')
+    except FileExistsError:
+        pass
 
-            if english != '':
-                file.write(
-                    f'{english}:{foreign};{foreign_caps};{foreign_all_caps}')
+    # Create the file
+    file = open(os.path.join('dict', f'{filename}.txt'), 'w')
+
+    # write a placeholder with 3 symbols to be replaced later by 3 digits
+    file.write('###\n')
+    total_words = 0
+
+    for row in rows:
+        columns = row.find_all('td')
+
+        if len(columns) == 4:
+            english = columns[0].get_text(strip=True)
+            foreign = columns[1].get_text(strip=True)
+            foreign_caps = columns[2].get_text(strip=True)
+            foreign_all_caps = columns[3].get_text(strip=True)
+        elif len(columns) == 3:
+            english = columns[0].get_text(strip=True)
+            foreign = columns[1].get_text(strip=True)
+            foreign_caps = columns[2].get_text(strip=True)
+            foreign_all_caps = ''
+        else:
+            english = ''
+            foreign = ''
+            foreign_caps = ''
+            foreign_all_caps = ''
+
+        if english != '':
+            file.write(f'{english}:{foreign};{foreign_caps};{foreign_all_caps}\n')
+            total_words += 1
+
+    file.seek(0)
+    file.write(f'{total_words:03d}')
+
+    file.close()
 
 
 if __name__ == '__main__':
